931f6d589c1424e813438c9bf32f3e3f638979cc
shakespearian1 <abiazaawari@gmail.com>
2024-11-25 18:17:21 +0200

    updated the entire repo by modifying the footer and the navbar, and you should try to download the ES7-React/Redux extention from vs code

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,19 +2,21 @@ import "/src/components/style/layout.css";
 import Nav from "./nav.jsx";
 import Body from "./body.jsx";
 import Footer from "./footer.jsx";
+import HomeHero from "./HomeHero.jsx";
 
-export default function Layout(){
-    return(
-        <div className="position">
-            <header className="top">
-                <Nav/>
-            </header>
-            <main className="middle">
-                <Body/>
-            </main>
-            <footer className="buttom">
-                <Footer/>
-            </footer>
-        </div>
-    )
+export default function Layout() {
+  return (
+    <div className="layout">
+      <div className="hero_nav">
+        <Nav />
+        <HomeHero />
+      </div>
+      <main className="middle">
+        <Body />
+      </main>
+      <footer className="buttom">
+        <Footer />
+      </footer>
+    </div>
+  );
 }
