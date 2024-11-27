fb0776fddae605f880a09f52a1b9a1a45a532c8e
muslmasan <muslmasan3@gmail.com>
2024-11-27 21:10:43 +0200

    i have edit the nav , adding the icon when it in the phone size and show the links in the disktop size + adding the show function to the js file so when i click the image the menu will appear , i edit the color of the nav and the footer + some edit to the button

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import Button from "./Button";
 import "/src/components/style/nav.css";
 
 export default function Nav() {
+  const [show,setShow] = useState(false)
   return (
     <>
-      <nav class="navbar">
-        <div class="logo-container">
+      <nav className="navbar">
+        <div className="logo-container">
+          <svg 
+          
+          onClick={()=>{ 
+            setShow(!show);
+            console.log(show);
+          }}
+          xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
           <div class="logo">Eduvaa</div>
         </div>
-        <menu class="nav-links">
+        <menu 
+        
+        className={show ? 'nav-links show' : 'nav-links'}>
           <ul>
+
             <li>
               <a href="#">Home</a>
             </li>
@@ -23,9 +35,12 @@ export default function Nav() {
               <a href="#">About Us</a>
             </li>
           </ul>
+        {/* } */}
+
         </menu>
         <Button text={"Sign Up"} />
       </nav>
     </>
   );
 }
+ 
